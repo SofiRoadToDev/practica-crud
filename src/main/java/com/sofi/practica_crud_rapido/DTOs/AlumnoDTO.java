2b60d373dffad594024d59d1320004803c440c48
SofiRoadToDev <sofi.workaffairs@gmail.com>
2024-10-09 13:33:15 -0300

    exceptionhandler

--- src/main/java/com/sofi/practica_crud_rapido/DTOs/AlumnoDTO.java
+++ src/main/java/com/sofi/practica_crud_rapido/DTOs/AlumnoDTO.java
@@ -1,5 +1,7 @@
 package com.sofi.practica_crud_rapido.DTOs;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +18,13 @@ public class AlumnoDTO {
 
 
     private Long id;
-
+    @Size(min= 1, max = 50, message = "El máximo de caracteres es 50")
     private String nombre;
 
+    @Size(min= 1, max = 50, message = "El máximo de caracteres es 50")
     private String apellido;
 
+    @Pattern(regexp = "\\d{8}", message = " El dni debe tener 8 dígitos, sin puntos")
     private String dni;
 
 }
---
+++ src/main/java/com/sofi/practica_crud_rapido/controllers/ControllerAdvice.java
@@ -0,0 +1,13 @@
+package com.sofi.practica_crud_rapido.controllers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@org.springframework.web.bind.annotation.ControllerAdvice( annotations = RestController.class)
+@Log4j2
+public class ControllerAdvice {
+
+
+
+}
